a3cc97d6a22030e5b840527a5251c8bea2c799cb
Sergio Esteban <Sergioss772022@gmail.com>
2024-02-14 02:48:37 +0100

    inital commit

---
+++ config/db.config.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const DB_NAME = "Onlyhack"
+const URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
+const DB_URI = `${URI}/${DB_NAME}`;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.info(`Successfully connected to the database ${DB_URI}`))
+  .catch((error) => {
+    console.error(
+      `An error ocurred trying to connect to de database ${DB_URI}`,
+      error
+    );
+    process.exit(0);
+  });
+
+// Cierre de la conexión
+
+process.on("SIGINT", () => {
+  mongoose.connection.close().then(function () {
+    console.log("Mongoose disconnected on app termination");
+    process.exit(0);
+  });
+});
